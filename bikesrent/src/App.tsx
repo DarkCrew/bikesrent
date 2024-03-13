3713bdd16d46382ada9f4776696bfce26d361394
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2024-03-13 17:16:11 +0100

    create: create app and intial

---
+++ bikesrent/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import './App.css';
+
+const App = (): ReactElement => {
+  return (
+    <div className="App">
+      <p>hello</p>
+    </div>
+  );
+};
+
+export default App;
